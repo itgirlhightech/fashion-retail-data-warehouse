30e3b81d2a1af33010544cc5daa3f11441e56960
Evilyn Feitosa <evlynfeitosa@gmail.com>
2026-07-16 17:22:52 -0300

    feat: add datetime transformations

--- src/transform.py
+++ src/transform.py
@@ -15,7 +15,20 @@ resultado_2 = resultado_2.rename(columns={"channel_x": "channel", "sale_date_x":
 resultado_3 = resultado_2.merge(datasets["products"], on="product_id")
 resultado_4 = resultado_3.merge(datasets["channels"], on="channel")
 
+#creating cost_total and profit columns 
 resultado_4["cost_total"]= resultado_4["cost_price"] * resultado_4["quantity"]
 resultado_4["profit"] = resultado_4["item_total"] - resultado_4["cost_total"]
 
-print(resultado_4["profit"])
+##resultado_4["discount"] = (
+ ## resultado_4["discount"].str.replace("%", "", regex=False)
+ ## .astype(float)
+ ##)
+
+resultado_4["sale_date"] = pd.to_datetime(resultado_4["sale_date"])
+resultado_4["year"] = resultado_4["sale_date"].dt.year
+resultado_4["month"] = resultado_4["sale_date"].dt.month
+resultado_4["quarter"] = resultado_4["sale_date"].dt.quarter
+resultado_4["week"] = resultado_4["sale_date"].dt.isocalendar().week
+resultado_4["day"] = resultado_4["sale_date"].dt.day
+
+print(resultado_4)
